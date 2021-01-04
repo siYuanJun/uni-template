d41b1b4dbe2569086eb11e346c51c4b6a9f0b628
siYuanJun <46435703+siYuanJun@users.noreply.github.com>
2021-01-04 14:05:06 +0800

    Update App.vue
    增加更新机制

--- App.vue
+++ App.vue
@@ -84,6 +84,37 @@
 		},
 		onShow: function() {
 			console.log('App Show')
+			// #ifdef MP-WEIXIN
+			if (uni.canIUse('getUpdateManager')) {
+				const updateManager = uni.getUpdateManager();
+				updateManager.onCheckForUpdate(function(res) {
+					// 请求完新版本信息的回调
+					if (res.hasUpdate) {
+						updateManager.onUpdateReady(function() {
+							uni.showModal({
+								title: '更新提示',
+								content: '新版本已经准备好，请重启应用！',
+								showCancel: false,
+								success: function(res) {
+									// res: {errMsg: "showModal: ok", cancel: false, confirm: true}
+									if (res.confirm) {
+										// 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+										updateManager.applyUpdate()
+									}
+								}
+							})
+						});
+						updateManager.onUpdateFailed(function() {
+							// 新的版本下载失败
+							uni.showModal({
+								title: '已经有新版本了哟~',
+								content: '新版本已经上线啦~，请您删除当前小程序，重新搜索打开哟~'
+							})
+						})
+					}
+				})
+			}
+			// #endif
 		},
 		onHide: function() {
 			console.log('App Hide')
