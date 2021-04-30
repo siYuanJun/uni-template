96f4a599368ace4ed6e1eb9774a03910575403e7
siYuanJun <2039750417@qq.com>
2021-04-30 13:17:30 +0800

    docs: commit

--- main.js
+++ main.js
@@ -6,10 +6,13 @@ import pagePull from "@/common/mixinPull";
 import share from "@/common/mixinShare";
 import routes from "@/common/routes";
 
-// git commit -m "feat: code"
-// git commit -m "fix: code"
-// git commit -m "perf: code audioPlay"
+// git commit -m "feat: code" // 新增一个功能
+// git commit -m "fix: code" // 修复一个 Bug
+// git commit -m "perf: code" // 改善性能
+// git commit -m "docs: commit" // 文档变更
+
 Vue.prototype.webTitle = init.config.title;
+// 版本号
 Vue.prototype.webVersion = init.config.version;
 // 项目token
 Vue.prototype.webTokey = init.config.webTokey;
